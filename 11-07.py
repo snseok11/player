8600cbd3c177809f9d46a8370f59bafef86ca6ca
snseok <snseok@DESKTOP-5HKHCHI>
2022-11-11 21:03:23 +0900

    first commit

---
+++ 11-07.py
@@ -0,0 +1,3 @@
+class Circle:
+    def __eq__(self, __o: object) -> bool:
+        pass
---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <typeinfo>
+
+using namespace std;
+
+template <typename T> T fnSquare(const T& a) {
+    return a*a;
+}
+
+template <typename T1, typename T2> T1 fnDivid(const T2& a, const T2& b) {
+    return static_cast<T1>(a)/static_cast<T1>(b);
+}
+
+template <typename T, size_t N = 2> T fnPower(const T& a) { 
+    T rst = 1;
+    for (size_t i = 0; i < N; i++, rst*=a);
+    return rst;
+}
+
+int main()
+{
+    // auto v1 = fnSquare(5);
+    // cout << v1 << ',' << typeid(v1).name() << endl;
+    // auto v2 = fnSquare<double>(5.1);
+    // cout << v2 << ',' << typeid(v2).name() << endl;
+    // cout << fnDivid<double, int>( 3, 5) << endl;
+    cout << fnPower(5) << endl;
+    cout << fnPower<int, 4>(5) << endl;
+    cout << fnPower<int, 3>(5) << endl;
+    cout << fnPower<int , 1>(5) << endl;
+}

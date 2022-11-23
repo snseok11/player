86f478d865dd37ccbb018ad0dae1710d9f81d011
snseok <snseok@DESKTOP-5HKHCHI>
2022-11-23 23:00:20 +0900

    2022-11-23

--- django_story/myapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-def index(request) :
-    return HttpResponse('welcome!')
-def create(request) :
-    return HttpResponse('create')
-def read(request, id) :
-    return HttpResponse('read' + id)
